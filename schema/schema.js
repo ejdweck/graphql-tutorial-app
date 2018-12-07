1dc50cb14d54c8c87723b17d26556f9ea2df6f92
ejdweck <evanjordandweck@gmail.com>
2018-12-06 18:11:29 -0600

    graphql up and running time to add a database to get rid of dummy data

--- schema/schema.js
+++ schema/schema.js
@@ -8,25 +8,41 @@ const {
   GraphQLSchema,
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
 } = graphql;
 
 // dummy data
 var notes = [
-  {title: 'Dry Cleaning', category: 'Home', id: '1', userId: '1'},
-  {title: 'Koa', category: 'Work', id: '1', userId: '1'},
-  {title: 'Winter', category: 'Buy', id: '1', userId: '1'},
-  {title: 'Dry', category: 'Home', id: '2', userId: '2'},
-  {title: 'Koa', category: 'Work', id: '2', userId: '2'},
-  {title: 'Winter', category: 'Buy', id: '2', userId: '2'},
-  {title: 'Dry', category: 'Home', id: '3', userId: '3'},
-  {title: 'Koa', category: 'Work', id: '3', userId: '3'},
+  {title: 'Get the plants watered', category: 'Home', id: '1', userId: '1'},
+  {title: 'email john', category: 'Work', id: '1', userId: '1'},
+  {title: 'sturdy leather boots', category: 'Buy', id: '1', userId: '1'},
+  {title: 'finish garage organization', category: 'Home', id: '2', userId: '2'},
+  {title: 'do API work', category: 'Work', id: '2', userId: '2'},
+  {title: 'frying pans for kitchen', category: 'Buy', id: '2', userId: '2'},
+  {title: 'carpet cleaning', category: 'Home', id: '3', userId: '3'},
+  {title: 'ask for raise', category: 'Work', id: '3', userId: '3'},
   {title: 'Winter Boots', category: 'Buy', id: '3', userId: '3'},
 ];
 var users = [
   {name: 'Bill', age: 28, id: '1'},
   {name: 'John', age: 18, id: '2'},
-  {title: 'Sandy', age: 22, id: '3'},
+  {name: 'Sandy', age: 22, id: '3'},
 ];
+
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    notes: {
+      type: new GraphQLList(NoteType),
+      resolve(parent, args) {
+        return _.filter(notes, {userId: parent.id})
+      }
+    }
+  })
+});
 
 const NoteType = new GraphQLObjectType({
   name: 'Note',
@@ -42,17 +58,6 @@ const NoteType = new GraphQLObjectType({
     }
   })
 });
-
-const UserType = new GraphQLObjectType({
-  name: 'User',
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    age: { type: GraphQLInt }
-  })
-});
-
-
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
@@ -70,6 +75,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(users, { id: args.id });
+      }
+    },
+    notes: {
+      type: new GraphQLList(NoteType),
+      resolve(parent, args) {
+        return notes;
+      }
+    },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parent, args) {
+        return users;
       }
     }
   }
